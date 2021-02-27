2f0f9fd9795a91d8b14b7dee1258694d8b996179
BrienB <barrdom@hotmail.com>
2021-02-27 10:29:42 -0500

    changed users_data and states_data table names to users and states

--- DB/schema.sql
+++ DB/schema.sql
@@ -4,15 +4,15 @@ CREATE DATABASE states_db;
 
 USE states_db;
 
-CREATE TABLE user_data
+CREATE TABLE users
 (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar (50) NOT NULL,
 	password varchar (50) NOT NULL,
 	PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE states_data
+CREATE TABLE states
 (
     id int NOT NULL AUTO_INCREMENT,
 	state_name varchar(50) NOT NULL,
