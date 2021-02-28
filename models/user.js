78429b711c134908a7898d90a18d5cb1273a5c2e
BrienB <barrdom@hotmail.com>
2021-02-28 11:25:09 -0500

    commented out code for db association

--- models/user.js
+++ models/user.js
@@ -16,7 +16,7 @@ module.exports = function(sequelize, DataTypes) {
     password: {
       type: DataTypes.STRING,
       allowNull: false
-    },
+    }
     // visitedStates: {
     //   type: DataTypes.STRING
     // },
